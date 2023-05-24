f1db2d73f9e9acd639a9a60f4d26335009b26ae8
James Richardson <james_e_richardson@me.com>
2023-05-24 18:36:18 +0100

    Project complete

--- bank_account.js
+++ bank_account.js
@@ -13,8 +13,47 @@ class BankAccount {
         const newTransaction = new Transaction(date, 0, amount)
         this.transactions.push(newTransaction);
     }
+
+    deposit(date, amount) {
+        const newTransaction = new Transaction(date, amount, 0)
+        this.transactions.push(newTransaction);
+    }
     
+    transactionsIncludingBalance() {
+        let balance = 0 
+        const transactionsIncludingBalance = [];
+
+        this.transactions.forEach(transaction => {
+            if(transaction.credit > 0) {
+                balance += transaction.credit;
+            } else {
+                balance -= transaction.debit;
+            }
+
+            transactionsIncludingBalance.push({
+                date: transaction.date, 
+                credit: transaction.credit,
+                debit: transaction.debit,
+                balance: balance
+            });
+    });
+
+        return transactionsIncludingBalance;
+    }
     
+    accountStatement() {
+        let statement = "date  ||  credit  ||  debit  ||  balance\n"
+
+        this.transactionsIncludingBalance().forEach(transaction => {
+            const date = transaction.date;
+            const credit = transaction.credit;
+            const debit = transaction.debit;
+            const balance = transaction.balance;
+        
+            statement += `${date}  ||  ${credit}  ||  ${debit}  ||  ${balance}\n`;
+          });
+          return statement;
+    }
 };
 
 module.exports = BankAccount; 
--- integration.test.js
+++ integration.test.js
@@ -15,4 +15,29 @@ describe('BankAccount', () => {
         expect(account.getTransactions().length).toEqual(1);
     });
 
+    test ('credit transaction is added to transactions array', () => {
+        const account = new BankAccount();
+        account.deposit("12/01/21", 1000)
+        expect(account.getTransactions().length).toEqual(1);
+    });
+
+    test ('returns the correct credit amount', () => {
+        const account = new BankAccount();
+        account.deposit("12/01/21", 1000);
+        const result = account.transactionsIncludingBalance();
+        expect(result[0].credit).toEqual(1000);
+    });
+
+    test ('produces an account statement based on all transactions', () => {
+        const account = new BankAccount();
+        account.deposit("10/01/2023", 1000);
+        account.deposit("13/01/2023", 2000);
+        account.withdraw("14/01/2023", 500);
+        const expectedAccountStatement = "date  ||  credit  ||  debit  ||  balance\n" +
+          "10/01/2023  ||  1000  ||  0  ||  1000\n" +
+          "13/01/2023  ||  2000  ||  0  ||  3000\n" +
+          "14/01/2023  ||  0  ||  500  ||  2500\n";
+        expect(account.accountStatement()).toContain(expectedAccountStatement);
+      });
+
 })
